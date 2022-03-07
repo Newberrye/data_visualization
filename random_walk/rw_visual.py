cda4daf628aab01311916e714504e74f1e89369b
EthanN <newberrydethan@gmail.com>
2022-03-07 14:08:32 -0600

    Finished random walk

--- random_walk/rw_visual.py
+++ random_walk/rw_visual.py
@@ -6,14 +6,25 @@ from random_walk import RandomWalk
 flag = True
 while flag:
     # Make a random walk.
-    rw = RandomWalk()
+    rw = RandomWalk(10_000)
 
     rw.fill_walk()
 
     # Plot the points in the walk
-    plt.style.use('classic')
-    fig, ax = plt.subplots()
-    ax.scatter(rw.x_values, rw.y_values, s=15)
+    plt.style.use('dark_background')
+    fig, ax = plt.subplots(figsize=(15, 9))
+    point_number = range(rw.num_points)
+    ax.scatter(rw.x_values, rw.y_values, c=point_number, cmap=plt.cm.Blues,
+               edgecolor='none', s=5)
+
+    # Emphasize the first and last points.
+    ax.scatter(0, 0, c='green', edgecolor='none', s=50)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolor='none',
+               s=50)
+
+    # Remove the axes.
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
 
     plt.show()
 
