e27f0500d37f727f4f84e9cc5f17bca37b119872
EthanN <newberrydethan@gmail.com>
2022-03-12 14:06:42 -0600

    Fixed a couple of typos in files

--- API/python_repos_visual.py
+++ API/python_repos_visual.py
@@ -1,7 +1,7 @@
 import requests
 
 from plotly.graph_objs import Bar
-from plotly import  offline
+from plotly import offline
 
 # Make an API call and store the response.
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
